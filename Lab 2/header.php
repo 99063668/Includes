a3f7f437495c567c9b02fdc3540a31a262328dbc
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-03-11 10:25:06 +0100

    Add files via upload

---
+++ Lab 2/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Lab 2 - Includes en require</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav>
+        <a href="onderwerp1.php">Pagina 1</a> 
+        <a href="onderwerp2.php">Pagina 2</a> 
+        <a href="onderwerp3.php">Pagina 3</a> 
+    </nav>
+
+    <header>
+        <h1>Onderwerp pagina</h1>
+        <p>Beschrijving onderwerp</p>
+    </header>
+
+</body>
+</html>
---
+++ Lab 2/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Lab 2 - Includes en require</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+	<?php
+    include "includes/header.php";
+  ?>
+
+
+	<!-- laad hier via php de juiste contentpagina in (vanuit de pages map) in. Welke geselecteerd moet worden kun je uit de URL halen (URL_Params).-->
+
+	
+	<!-- laad hier via php je footer in (vanuit je includes map)-->
+
+
+</body>
+</html>
