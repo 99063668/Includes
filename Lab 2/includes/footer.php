25a89c5b5433e5563712c372f56bf143a0c70faa
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2021-03-15 12:25:11 +0100

    Add files via upload

---
+++ Lab 2/includes/footer.php
@@ -0,0 +1,20 @@
+<?php
+    $today = date("d.m.y");
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Lab 2 - Includes en require</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <footer>
+            <p> Dewi van der Velden @<?php echo $today; ?> </p>
+        </footer>
+    </div>
+
+</body>
+</html>
---
+++ Lab 2/includes/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Lab 2 - Includes en require</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <ul>
+                <li><a href="index.php?pages=Call_of_Duty">Call of Duty</a></li>
+                <li><a href="index.php?pages=God_of_War">God of War</a></li>
+                <li><a href="index.php?pages=Beat_Saber">Beat Saber</a></li>
+                <li><a href="index.php?pages=Tekken_7">Tekken 7</a></li>
+            </ul>
+        </nav>
+    </div>
+
+</body>
+</html>
